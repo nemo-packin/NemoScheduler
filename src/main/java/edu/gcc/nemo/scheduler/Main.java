9286323d82c63e684d5ba713a27f28dc4e7f023e
JUDGEPM20 <pjudge2002@gmail.com>
2023-02-22 14:49:00 -0500

    ello

--- src/main/java/edu/gcc/nemo/scheduler/Main.java
+++ src/main/java/edu/gcc/nemo/scheduler/Main.java
@@ -2,10 +2,14 @@ package edu.gcc.nemo.scheduler;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello nemo packers!")
+        System.out.println("Hello nemo packers!");
 
     }
     public static void dumb() {
         System.out.println("HI!");
+    }
+
+    public static void notDumb(){
+        System.out.println("Hello");
     }
 }
