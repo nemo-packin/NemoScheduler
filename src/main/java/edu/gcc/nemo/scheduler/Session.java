6adaed3c32e3216890c0652e35ee0a93318bed09
RAINEEN20 <raineen20@gcc.edu>
2023-02-28 15:40:17 -0500

    Stubs for enums and Session object(driver)

---
+++ src/main/java/edu/gcc/nemo/scheduler/Session.java
@@ -0,0 +1,20 @@
+package edu.gcc.nemo.scheduler;
+
+public class Session {
+    User user;
+
+    void authenticate() {}
+
+    void saveSchedule() {}
+
+    void editSchedule() {}
+
+    String searchStudents() {return null;}
+
+    String searchCourses() {return null;}
+
+    String getSchedule() {return null;}
+
+    String getStatusSheet() {return null;}
+
+}
