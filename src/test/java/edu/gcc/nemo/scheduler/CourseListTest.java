93f45ea654b1efe40ccff2da859c3e64b1b256b8
JUDGEPM20 <pjudge2002@gmail.com>
2023-03-20 14:42:37 -0400

    made a test file

---
+++ src/test/java/edu/gcc/nemo/scheduler/CourseListTest.java
@@ -0,0 +1,17 @@
+package edu.gcc.nemo.scheduler;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseListTest {
+
+    @Test
+    void addCourse() {
+
+    }
+
+    @Test
+    void removeCourse() {
+    }
+}
