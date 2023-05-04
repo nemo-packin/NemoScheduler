a495f7e5bc88f37d0502bde486c627ab1103d571
dytryk <astonmartinone111111@gmail.com>
2023-05-04 12:28:04 -0400

    regex done

--- src/main/java/edu/gcc/nemo/scheduler/util/MinorParser.java
+++ src/main/java/edu/gcc/nemo/scheduler/util/MinorParser.java
@@ -1,39 +1,28 @@
 package edu.gcc.nemo.scheduler.util;
 
-import com.google.gson.Gson;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-public class MinorParser
-{
+public class MinorParser {
     public static void main(String[] args) {
         ClassLoader c = Thread.currentThread().getContextClassLoader();
         InputStream inputStream = c.getResourceAsStream("Minors 2022-23.txt");
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
         String f = br.lines().collect(Collectors.joining("\n"));
-        System.out.println(f);
-        Pattern minorTitle = Pattern.compile("\\b[A-Z & and -]+\\.", Pattern.DOTALL | Pattern.CASE_INSENSITIVE);
-        System.out.println(minorTitle);
+        String pattern = "(^[ +A-Z& and-]+)\\. ([0-9-]+) required [a-z ,:]+(.+?\\.+[^A-Z a-z ,.;:/]\\n)";
+        Pattern r = Pattern.compile(pattern, Pattern.MULTILINE | Pattern.DOTALL);
+        Matcher m = r.matcher(f);
 
-
-        String regex = "(\\p{Upper}[\\p{Upper}\\p{Lower}\\s&]+)\\.\\s+(\\d+\\s+required\\s+hours,\\s+including\\s+.+?\\.)";
-        Pattern pattern = Pattern.compile(regex, Pattern.DOTALL);
-
-
-        // Create a Matcher object
-        Matcher matcher = pattern.matcher(regex);
-
-        // Loop through the matches and print the minor name and course requirements
-        while (matcher.find()) {
-            String minorName = matcher.group(1);
-            String courseRequirements = matcher.group(2);
-            System.out.println("Minor Name: " + minorName);
-            System.out.println("Course Requirements: " + courseRequirements);
+        while (m.find()) {
+            String title = m.group(1);
+            String hours = m.group(2);
+            String courses = m.group(3).trim();
+            System.out.println("Title: " + title);
+            System.out.println("Hours: " + hours);
+            System.out.println("Courses: " + courses);
+            System.out.println();
         }
     }
 }
